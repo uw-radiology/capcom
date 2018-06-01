9b20579f697f1ef6c1c499ac3e47d6489e13cace
Steven Beshensky <s.beshensky@gmail.com>
2018-06-01 16:30:11 -0400

    solve cors problem

--- src/image-service.js
+++ src/image-service.js
@@ -1,10 +1,13 @@
 const getStudies = async () => {
-  return await fetch('www.google.com', {
-    headers: {
-      'content-type': 'application/json'
-    },
-    method: 'GET'
-  });
+  const result = await fetch('https://hackathon.siim.org/dicomweb/studies/', {
+      headers: {
+        'content-type': 'application/json',
+        'apikey':'58b70407-7431-4973-b889-35408646eae9',
+        'Accept':'application/json'
+      },
+      method: 'GET'
+    })
+    return await result.json();
 }
 
 export {
